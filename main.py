0cb80724eac575df33b7fb7a568ecf42ce662c5f
Ian Aquino <aquinoian@icloud.com>
2023-05-01 17:49:34 +0100

    added return type using pydantic model

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
+from typing import List
 from fastapi import FastAPI
 
 from database import call_database
+from title import TitleBasic
 
 app = FastAPI()
 
 @app.get("/api/titles")
-async def get_all_titles():
+async def get_all_titles() -> List[TitleBasic]:
     try:
         # Mock database call
         titles_data = await call_database()
