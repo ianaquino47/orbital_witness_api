0a63341cc1a4a0ac576edbf9f656f02e487867bf
Ian Aquino <aquinoian@icloud.com>
2023-05-01 18:50:23 +0100

    added validation for sort/order params

--- utils.py
+++ utils.py
@@ -1,8 +1,11 @@
 from typing import List, Union
 from title import TitleBasic
 
+VALID_SORT_PARAMETERS = ('id', 'title_number', 'title_class')
+VALID_ORDER_PARAMETERS = ('asc', 'desc')
 
-def filter_by_title_class(data: List[TitleBasic], title_class: Union[str, None]) -> List:
+
+def filter_by_title_class(data: List[TitleBasic], title_class: Union[str, None]) -> List[TitleBasic]:
     if title_class:
         filtered_titles = [
             title for title in data if title.title_class.lower() == title_class.lower()]
@@ -11,15 +14,28 @@ def filter_by_title_class(data: List[TitleBasic], title_class: Union[str, None])
     return filtered_titles
 
 
-def get_sort_keys(sort_params: List, order_params: List) -> List:
+def get_sort_keys(sort_params: List[str], order_params: List[str]) -> List[tuple]:
     sort_keys = []
     for (sort_param, order_param) in (zip(sort_params, order_params)):
+        validate_parameter(sort_param, VALID_SORT_PARAMETERS)
+        validate_parameter(order_param, VALID_ORDER_PARAMETERS)
         reverse = order_param.lower() == "desc"
         sort_keys.append((sort_param, reverse))
     return sort_keys
 
 
-def sort_data(data: List, sort_keys: List) -> List:
+def validate_parameter(param: str, valid_parameters: tuple) -> None:
+    if (param not in valid_parameters):
+        raise CustomException(
+            code=404,
+            response=response(
+                success=False,
+                message=f'Invalid paramater: {param}'
+            )
+        )
+
+
+def sort_data(data: List, sort_keys: List[tuple]) -> List[TitleBasic]:
     sorted_titles = data
     if sort_keys:
         # sorting is stable, hence we can do it in reverse order
@@ -34,7 +50,7 @@ def sort_data(data: List, sort_keys: List) -> List:
     return sorted_titles
 
 
-def get_paginated_data(data: List, page: int, limit: int) -> List[TitleBasic]:
+def get_paginated_data(data: List[TitleBasic], page: int, limit: int) -> List[TitleBasic]:
     if limit:
         start_index = (page - 1) * limit
         end_index = start_index + limit
@@ -43,10 +59,12 @@ def get_paginated_data(data: List, page: int, limit: int) -> List[TitleBasic]:
         paginated_titles = data
     return paginated_titles
 
+
 class CustomException(Exception):
     def __init__(self, response: dict, code: int):
         self.response = response
         self.code = code
+
 
 def response(success: bool = True, message: str = '', data: dict = {}) -> dict:
     res = {'success': success, 'data': data}
