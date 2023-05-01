58eebf9d52911b47f494f082049f20634418fa08
Ian Aquino <aquinoian@icloud.com>
2023-05-01 18:17:12 +0100

    added sorting feature

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ from typing import List
 from fastapi import FastAPI, Query
 from database import call_database
 from title import TitleBasic
-from utils import filter_by_title_class
+from utils import filter_by_title_class, get_sort_keys, sort_data
 
 app = FastAPI()
 
 @app.get("/api/titles")
 async def get_all_titles(
     title_class: str = Query(None),
+    _sort: str = Query(None),
+    _order: str = Query('asc'),
 ) -> List[TitleBasic]:
     try:
         # Mock database call
@@ -16,6 +18,16 @@ async def get_all_titles(
 
         # Filter by title_class if specified
         filtered_titles = filter_by_title_class(titles_data, title_class)
+
+        # Parse _sort and _order query parameters
+        sort_params = _sort.split(",") if _sort else []
+        order_params = _order.split(",") if _order else []
+
+        # Create a list of tuples (key, reverse) to pass to sorted()
+        sort_keys = get_sort_keys(sort_params, order_params)
+
+        # Sort the list using multiple keys (default = asc)
+        sorted_titles = sort_data(filtered_titles, sort_keys)
         return filtered_titles
     except:
         raise
--- utils.py
+++ utils.py
@@ -9,3 +9,24 @@ def filter_by_title_class(data: List[TitleBasic], title_class: Union[str, None])
     else:
         filtered_titles = data
     return filtered_titles
+
+def get_sort_keys(sort_params: List, order_params: List) -> List:
+    sort_keys = []
+    for (sort_param, order_param) in (zip(sort_params, order_params)):
+        reverse = order_param.lower() == "desc"
+        sort_keys.append((sort_param, reverse))
+    return sort_keys
+
+def sort_data(data: List, sort_keys: List) -> List:
+    sorted_titles = data
+    if sort_keys:
+        # sorting is stable, hence we can do it in reverse order
+        for sort_param, order_param in reversed(sort_keys):
+            sorted_titles.sort(
+                # id is a string hence we need to parse it for correct sorting
+                key=lambda title: int(
+                    getattr(title, sort_param)) if sort_param == 'id' else getattr(title, sort_param), 
+                reverse=order_param
+            )
+
+    return sorted_titles
